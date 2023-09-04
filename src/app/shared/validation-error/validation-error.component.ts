3918d2f03b68e2664250aa37505516b24aac4e44
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-09-04 11:35:35 +0200

    Add login form validation

---
+++ src/app/shared/validation-error/validation-error.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validation-error',
+  templateUrl: './validation-error.component.html',
+  styleUrls: ['./validation-error.component.scss'],
+})
+export class ValidationErrorComponent {}
