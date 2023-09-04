02c1805ed9e3386b6164a8146f650bd528e67fa9
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-09-04 10:25:28 +0200

    Add tailwind & primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    DropdownModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+        mono: ['Ubuntu Mono', ...defaultTheme.fontFamily.mono],
+      },
+      maxWidth: {
+        page: '1360px',
+      },
+      backgroundColor: {
+        ...colors,
+        dark: '#090A10',
+        light: '#0A0C14',
+        gray: colors.slate,
+        primary: colors.blue,
+        success: colors.green,
+        danger: colors.red,
+        warning: colors.yellow,
+        info: colors.blue,
+      },
+      colors: {
+        ...colors,
+        gray: colors.slate,
+        primary: colors.blue,
+        success: colors.green,
+        danger: colors.red,
+        warning: colors.yellow,
+        info: colors.blue,
+        text: {
+          primary: '#fff',
+          secondary: colors.slate[400],
+          tertiary: colors.slate[500],
+        },
+        bg: {
+          dark: '#090A10',
+          light: '#0A0C14',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
