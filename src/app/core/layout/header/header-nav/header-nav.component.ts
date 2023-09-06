9dcc53f1e60f7c9531a3f1381f3a343811714c7e
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-09-06 23:24:57 +0200

    Add basic user dropdown menu

--- src/app/core/layout/header/header-nav/header-nav.component.ts
+++ src/app/core/layout/header/header-nav/header-nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '@core/services/auth/auth.service';
 import { Subscription } from 'rxjs';
+import { JwtService } from '@core/services/auth/jwt.service';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header-nav',
@@ -9,12 +11,32 @@ import { Subscription } from 'rxjs';
 })
 export class HeaderNavComponent implements OnInit, OnDestroy {
   authSub?: Subscription;
-  isLoggedIn: boolean = false;
+  userDropdownItems?: MenuItem[];
 
-  constructor(private authService: AuthService) {}
+  isLoggedIn: boolean = false;
+  username: string | null = null;
+  usernameFirstLetter: string | null = null;
+
+  constructor(
+    private authService: AuthService,
+    private jwtService: JwtService,
+  ) {}
 
   ngOnInit(): void {
     this.handleAuth();
+    this.handleSetUserDropdownItems();
+  }
+
+  handleSetUserDropdownItems() {
+    this.userDropdownItems = [
+      {
+        label: 'Logout',
+        icon: 'pi pi-sign-out',
+        command: (_) => {
+          this.authService.logout();
+        },
+      },
+    ];
   }
 
   handleAuth() {
@@ -22,15 +44,24 @@ export class HeaderNavComponent implements OnInit, OnDestroy {
     this.authSub = this.authService.isAuthObservable().subscribe({
       next: (isAuth) => {
         this.isLoggedIn = isAuth;
+        this.setUsername();
       },
     });
+
+    this.setUsername();
+  }
+
+  setUsername() {
+    if (this.isLoggedIn) {
+      this.username = this.jwtService.getUsername();
+      this.usernameFirstLetter = this.username?.charAt(0) || null;
+    } else {
+      this.username = null;
+      this.usernameFirstLetter = null;
+    }
   }
 
   ngOnDestroy(): void {
     this.authSub?.unsubscribe();
-  }
-
-  handleLogout() {
-    this.authService.logout();
   }
 }
--- src/app/core/layout/layout.module.ts
+++ src/app/core/layout/layout.module.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from './header/header.component';
 import { HeaderNavComponent } from './header/header-nav/header-nav.component';
 import { HeaderNavLinkComponent } from './header/header-nav-link/header-nav-link.component';
 import { RouterLink } from '@angular/router';
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from '@app/shared/shared.module';
+import { AvatarModule } from 'primeng/avatar';
+import { MenuModule } from 'primeng/menu';
 
 @NgModule({
   declarations: [HeaderComponent, HeaderNavComponent, HeaderNavLinkComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule, RouterLink, SharedModule],
+  imports: [CommonModule, RouterLink, SharedModule, AvatarModule, MenuModule],
 })
 export class LayoutModule {}
