d3b280f230994be59d2a0c7bc08feca1f27a30c9
Marcin Lisiecki <marcinlisiecki9@gmail.com>
2023-09-25 15:55:50 +0200

    Add sticky header

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isHeaderSmall: boolean = false;
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', () => this.changeHeaderState());
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', () => this.changeHeaderState());
+  }
+
+  changeHeaderState() {
+    if (!this.isHeaderSmall && window.scrollY > 0) {
+      this.isHeaderSmall = true;
+    } else if (this.isHeaderSmall && window.scrollY === 0) {
+      this.isHeaderSmall = false;
+    }
+  }
 }
